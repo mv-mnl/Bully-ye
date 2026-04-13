6a89d30ddbde4c5ccee4c5cf013e5a29507c6118
mv-mnl <mmelendez1505@gmail.com>
2026-04-13 11:46:58 -0600

    feat

--- hypr/scripts/detect_machine.sh
+++ hypr/scripts/detect_machine.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-if [ -d /sys/class/power_supply/BAT0 ] || [ -d /sys/class/power_supply/BAT1 ]; then
-    echo "laptop"
+# Dentro de tu script de instalación/enlaces
+MACHINE_TYPE=$(bash ~/Bully-ye/hypr/scripts/detect_machine.sh)
+
+if [ "$MACHINE_TYPE" == "laptop" ]; then
+    ln -sf ~/Bully-ye/hypr/laptop.conf ~/Bully-ye/hypr/machine.conf
 else
-    echo "desktop"
+    ln -sf ~/Bully-ye/hypr/desktop.conf ~/Bully-ye/hypr/machine.conf
 fi
